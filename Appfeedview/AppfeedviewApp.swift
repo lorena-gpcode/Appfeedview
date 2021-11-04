45145d3cd71d3f975794a574bc876b322e92d28d
lorena <lorenaggama@gmail.com>
2021-11-04 11:18:31 -0300

    Initial Commit

---
+++ Appfeedview/AppfeedviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppfeedviewApp.swift
+//  Appfeedview
+//
+//  Created by Lorena Gama on 04/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AppfeedviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appfeedview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appfeedview
+//
+//  Created by Lorena Gama on 04/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
